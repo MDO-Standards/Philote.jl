3ee090cc771bed1e1cb2a91ccd111e35cae4640a
Christopher Lupp <christopherlupp@gmail.com>
2025-11-02 19:21:25 -0500

    Add implicit discipline wrapper implementation
    - Create JuliaImplicitWrapperDiscipline class for implicit disciplines
    - Implement compute_residuals, solve_residuals, and residual_partials methods
    - Add support for residuals in setup via add_residual
    - Import ImplicitDiscipline from Philote-Python
    - Export JuliaImplicitWrapperDiscipline in package __init__

--- philote_julia/__init__.py
+++ philote_julia/__init__.py
@@ -7,11 +7,12 @@ Philote gRPC server infrastructure.
 
 __version__ = "0.1.0"
 
-from .wrapper_discipline import JuliaWrapperDiscipline
+from .wrapper_discipline import JuliaWrapperDiscipline, JuliaImplicitWrapperDiscipline
 from .config import DisciplineConfig, ServerConfig, PhiloteConfig
 
 __all__ = [
     "JuliaWrapperDiscipline",
+    "JuliaImplicitWrapperDiscipline",
     "DisciplineConfig",
     "ServerConfig",
     "PhiloteConfig",
